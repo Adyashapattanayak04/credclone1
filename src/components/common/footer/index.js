b4f04e18c57f3ba785401bd53677a301e23a0e82
Adyashapattanayak04 <adyasha.pattanayak00@gmail.com>
2022-03-24 20:44:18 +0530

    commit message

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer flex absolute-center max-width">
+      <span>&copy;</span> Made by Adyasha
+    </div>
+  );
+};
+
+export default Footer;
